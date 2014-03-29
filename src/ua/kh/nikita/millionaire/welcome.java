e7258b761abf55231938d5f7dd0ab5ed0f5efcda
Odarchenko N.D <odarchenko.n.d@gmail.com>
2014-03-29 16:55:35 +0200

    base activity

--- src/ua/kh/nikita/millionaire/welcome.java
+++ src/ua/kh/nikita/millionaire/welcome.java
@@ -2,14 +2,30 @@ package ua.kh.nikita.millionaire;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class welcome extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // start new game
+            }
+        });
+
+        final Button button1 = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // exit
+                finish();
+                System.exit(0);
+            }
+        });
     }
 }
